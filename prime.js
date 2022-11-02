c5c7754bcc27cee56e61e2ff2cb1e706767654c0
roy <roy19rishabh@gmail.com>
2022-11-02 12:17:40 +0530

    fixed the code by Rishabh

--- prime.js
+++ prime.js
@@ -1,16 +1,18 @@
 function checkPrime(num){
 let count=0;
-for (let i=i; i<=num; i++ ){
-if (num%i){
+for (let i=1; i<=num; i++ ){
+if (num%i === 0){
 count++;
+}
 }
 if (count==2){
 return true;
-}
+}else{
 return false;
 }
+}
 
-let ans= checkPrime(17)
+let ans= checkPrime(12)
 if (ans==true){
 console.log("Prime");
 
